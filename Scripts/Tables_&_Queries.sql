e285404db6a9322b40e2930a4d54a80f9d92f80b
ZachKopec <zachkopec@optonline.net>
2020-11-04 20:14:32 -0500

    Last two queries completed

--- Scripts/Tables_&_Queries.sql
+++ Scripts/Tables_&_Queries.sql
@@ -39,33 +39,33 @@ CREATE TABLE titles (
 
 SELECT * FROM dept_manager;
 
-SELECT emp.emp_no
-	  ,emp.last_name
-	  ,emp.first_name
-	  ,emp.sex
-	  ,sal.salary
+SELECT emp.emp_no AS Employee_ID
+	  ,emp.last_name AS Employee_Last_Name
+	  ,emp.first_name AS Employee_First_Name
+	  ,emp.sex AS Employee_Sex
+	  ,sal.salary AS Employee_Salary
 FROM employees AS emp
 INNER JOIN salaries AS sal ON emp.emp_no = sal.emp_no;
 
-SELECT first_name
-	  ,last_name
-	  ,hire_date
+SELECT first_name AS Employee_First_Name
+	  ,last_name AS Employee_Last_Name
+	  ,hire_date AS Employee_Hire_Date
 FROM employees
 	WHERE DATE_PART('YEAR', hire_date) = 1986;
 	
-SELECT dm.dept_no
-      ,dept.dept_name
-	  ,dm.emp_no
-	  ,emp.last_name
-	  ,emp.first_name
+SELECT dm.dept_no AS Department_ID
+      ,dept.dept_name AS Department_Name
+	  ,dm.emp_no AS Employee_ID
+	  ,emp.last_name AS Employee_Last_Name
+	  ,emp.first_name AS Employee_First_Name
 FROM dept_manager AS DM
 INNER JOIN department AS dept ON DM.dept_no = dept.dept_no
 INNER JOIN employees AS emp ON DM.emp_no = emp.emp_no;
 
-SELECT emp.emp_no
-	  ,emp.last_name
-	  ,emp.first_name
-	  ,dept.dept_name
+SELECT emp.emp_no AS Employee_ID
+	  ,emp.last_name AS Employee_Last_Name
+	  ,emp.first_name AS Employee_First_Name
+	  ,dept.dept_name AS Department_Name
 FROM employees AS emp
 INNER JOIN dept_emp AS DE ON emp.emp_no = DE.emp_no
 INNER JOIN department AS dept ON DE.dept_no = dept.dept_no;
@@ -85,6 +85,22 @@ FROM employees AS emp
 INNER JOIN dept_emp AS DE ON emp.emp_no = DE.emp_no
 INNER JOIN department AS dept ON DE.dept_no = dept.dept_no
 	WHERE dept.dept_name = 'Sales';
+	
+SELECT emp.emp_no
+	  ,emp.last_name
+	  ,emp.first_name
+	  ,dept.dept_name
+FROM employees AS emp
+INNER JOIN dept_emp AS DE ON emp.emp_no = DE.emp_no
+INNER JOIN department AS dept ON DE.dept_no = dept.dept_no
+	WHERE dept.dept_name = 'Sales'
+		OR dept.dept_name = 'Development';
+		
+SELECT last_name
+	  ,COUNT(last_name) AS Last_Name_Count
+FROM employees
+	GROUP BY last_name
+	ORDER BY COUNT(last_name) DESC;
 		
 
 
